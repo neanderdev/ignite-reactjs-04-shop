8d99eae2b250ae5a53e22851178da683c79069f2
Neander de Souza <neander.faria@hotmail.com>
2023-06-27 14:47:39 -0300

    Trabalhando com SEO

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { GetStaticPaths, GetStaticProps } from "next";
+import Head from 'next/head';
 import Image from "next/image";
 import { useState } from "react";
 import Stripe from "stripe";
@@ -41,23 +42,29 @@ export default function Product({ product }: ProductProps) {
     }
 
     return (
-        <ProductContainer>
-            <ImageContainer>
-                <Image src={product.imageUrl} width={520} height={480} alt="" />
-            </ImageContainer>
+        <>
+            <Head>
+                <title>{product.name} | Ignite Shop</title>
+            </Head>
 
-            <ProductDetails>
-                <h1>{product.name}</h1>
+            <ProductContainer>
+                <ImageContainer>
+                    <Image src={product.imageUrl} width={520} height={480} alt="" />
+                </ImageContainer>
 
-                <span>{product.price}</span>
+                <ProductDetails>
+                    <h1>{product.name}</h1>
 
-                <p>{product.description}</p>
+                    <span>{product.price}</span>
 
-                <button disabled={isCreatingCheckoutSession} onClick={handleBuyButton}>
-                    Comprar agora
-                </button>
-            </ProductDetails>
-        </ProductContainer>
+                    <p>{product.description}</p>
+
+                    <button disabled={isCreatingCheckoutSession} onClick={handleBuyButton}>
+                        Comprar agora
+                    </button>
+                </ProductDetails>
+            </ProductContainer>
+        </>
     )
 }
 
