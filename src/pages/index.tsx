540c8390dad9f4120a325347900e6540e0287a2c
Neander de Souza <neander.faria@hotmail.com>
2023-06-27 14:25:06 -0300

    Navegação via Link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import 'keen-slider/keen-slider.min.css'
 import { useKeenSlider } from 'keen-slider/react'
 import { GetStaticProps } from "next"
 import Image from "next/image"
+import Link from "next/link"
 import Stripe from "stripe"
 
 import { stripe } from "../lib/stripe"
@@ -29,15 +30,19 @@ export default function Home({ products }: HomeProps) {
         <HomeContainer ref={sliderRef} className="keen-slider">
             {products.map(product => {
                 return (
-                    <Product key={product.id} className="keen-slider__slide">
-                        <Image src={product.imageUrl} width={520} height={480} alt="" priority />
+                    <Link href={`/product/${product.id}`} key={product.id}>
+                        <Product
+                            className="keen-slider__slide"
+                        >
+                            <Image src={product.imageUrl} width={520} height={480} alt="" />
 
-                        <footer>
-                            <strong>{product.name}</strong>
+                            <footer>
+                                <strong>{product.name}</strong>
 
-                            <span>{product.price}</span>
-                        </footer>
-                    </Product>
+                                <span>{product.price}</span>
+                            </footer>
+                        </Product>
+                    </Link>
                 )
             })}
         </HomeContainer>
