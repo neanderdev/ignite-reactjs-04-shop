56c9ab3f6c183a0546592c7b3b30553f9fa04707
Neander de Souza <neander.faria@hotmail.com>
2023-06-27 13:53:22 -0300

    Imagens no Next.js

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from "next/app"
+import Image from "next/image"
 
 import { globalStyles } from "../styles/global"
 import { Container, Header } from "../styles/pages/app"
@@ -11,7 +12,7 @@ function App({ Component, pageProps }: AppProps) {
     return (
         <Container>
             <Header>
-                <img src={logoImg.src} alt="" />
+                <Image src={logoImg} alt="" />
             </Header>
 
             <Component {...pageProps} />
