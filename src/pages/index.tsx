fc104e3a2e5901d8568e0c1df2e846976f0a47dd
Neander de Souza <neander.faria@hotmail.com>
2023-06-27 14:05:53 -0300

    Data Fetching no Next.js

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,8 +7,9 @@ import { HomeContainer, Product } from "../styles/pages/home"
 import camiseta1 from "../assets/camisetas/1.png"
 import camiseta2 from "../assets/camisetas/2.png"
 import camiseta3 from "../assets/camisetas/3.png"
+import camiseta4 from "../assets/camisetas/4.png"
 
-export default function Home() {
+export default function Home(props) {
     const [sliderRef] = useKeenSlider({
         slides: {
             perView: 3,
@@ -18,6 +19,8 @@ export default function Home() {
 
     return (
         <HomeContainer ref={sliderRef} className="keen-slider">
+            <pre>{JSON.stringify(props.list)}</pre>
+
             <Product className="keen-slider__slide">
                 <Image src={camiseta1} width={520} height={480} alt="" />
 
@@ -49,7 +52,7 @@ export default function Home() {
             </Product>
 
             <Product className="keen-slider__slide">
-                <Image src={camiseta3} width={520} height={480} alt="" />
+                <Image src={camiseta4} width={520} height={480} alt="" />
 
                 <footer>
                     <strong>Camiseta X</strong>
@@ -59,4 +62,16 @@ export default function Home() {
             </Product>
         </HomeContainer>
     )
+}
+
+export const getServerSideProps = async () => {
+    await new Promise(resolve => setTimeout(resolve, 2000))
+
+    console.log('rodou')
+
+    return {
+        props: {
+            list: [1, 2, 3]
+        }
+    }
 }
